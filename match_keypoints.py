1040a2784d18b58918382e04372373e63d511c4d
Edgar <emroman@stanford.edu>
2023-03-22 16:06:28 -0700

    Matched correspondences

--- match_keypoints.py
+++ match_keypoints.py
@@ -6,27 +6,26 @@ import os
 
 DATA_DIR = './data/statue/images/dslr_images_undistorted/'
 
-
 if __name__ == '__main__':
-    img1 = cv.imread(os.path.join(DATA_DIR, "DSC_0490.JPG"), cv.IMREAD_GRAYSCALE)          # queryImage
-    img2 = cv.imread(os.path.join(DATA_DIR, "DSC_0490.JPG"), cv.IMREAD_GRAYSCALE)  # trainImage
-    # Initiate SIFT detector
+    img1 = cv.imread(os.path.join(DATA_DIR, "DSC_0490.JPG"), cv.IMREAD_GRAYSCALE)
+    img2 = cv.imread(os.path.join(DATA_DIR, "DSC_0490.JPG"), cv.IMREAD_GRAYSCALE)
+
     sift = cv.SIFT_create()
 
-    # find the keypoints and descriptors with SIFT
     kp1, des1 = sift.detectAndCompute(img1, None)
     kp2, des2 = sift.detectAndCompute(img2, None)
 
-    # BFMatcher with default params
     bf = cv.BFMatcher()
     matches = bf.knnMatch(des1, des2, k=2)
 
-    # Apply ratio test
-    good = []
-    for m, n in matches:
-        if m.distance < 0.75*n.distance:
-            good.append([m])
+    good = [[m] for m, n in matches if m.distance < 0.75 * n.distance]
+
+    query = np.array([kp1[g[0].queryIdx].pt for g in good], dtype=np.float32)
+    train = np.array([kp2[g[0].trainIdx].pt for g in good], dtype=np.float32)
+
+    np.save('query.npy', query)
+    np.save('train.npy', train)
 
     # cv.drawMatchesKnn expects list of lists as matches.
-    img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    plt.imshow(img3), plt.show()
+    # img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    # plt.imshow(img3), plt.show()
