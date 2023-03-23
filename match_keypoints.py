6a745cc1efa800d2911ead75a784821603bb42c9
Edgar <emroman@stanford.edu>
2023-03-22 17:45:44 -0700

    Matched correspondences

--- match_keypoints.py
+++ match_keypoints.py
@@ -22,14 +22,40 @@ if __name__ == '__main__':
     bf = cv.BFMatcher()
     matches = bf.knnMatch(des1, des2, k=2)
 
-    good = [[m] for m, n in matches if m.distance < 0.75 * n.distance]
+    good = []
+    for m, n in matches:
+        if m.distance < 0.7 * n.distance:
+            good.append(m)
 
-    query = np.array([kp1[g[0].queryIdx].pt for g in good], dtype=np.float32)
-    train = np.array([kp2[g[0].trainIdx].pt for g in good], dtype=np.float32)
+    MIN_MATCH_COUNT = 10
 
-    np.save('query.npy', query)
-    np.save('train.npy', train)
+    if len(good) > MIN_MATCH_COUNT:
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+        M, mask = cv.findHomography(src_pts, dst_pts, cv.RANSAC, 5.0)
+        print(M)
+        matchesMask = mask.ravel().tolist()
+        h, w = img1.shape
+        pts = np.float32([[0, 0], [0, h - 1], [w - 1, h - 1], [w - 1, 0]]).reshape(-1, 1, 2)
+        dst = cv.perspectiveTransform(pts, M)
+        img2 = cv.polylines(img2, [np.int32(dst)], True, 255, 3, cv.LINE_AA)
+    else:
+        print("Not enough matches are found - {}/{}".format(len(good), MIN_MATCH_COUNT))
+        matchesMask = None
 
-    # cv.drawMatchesKnn expects list of lists as matches.
-    img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-    plt.imshow(img3), plt.show()
+    draw_params = dict(matchColor=(0, 255, 0),  # draw matches in green color
+                       singlePointColor=None,
+                       matchesMask=matchesMask,  # draw only inliers
+                       flags=2)
+    img3 = cv.drawMatches(img1, kp1, img2, kp2, good, None, **draw_params)
+    plt.imshow(img3, 'gray'), plt.show()
+
+    # query = np.array([kp1[g[0].queryIdx].pt for g in good], dtype=np.float32)
+    # train = np.array([kp2[g[0].trainIdx].pt for g in good], dtype=np.float32)
+    #
+    # np.save('query.npy', query)
+    # np.save('train.npy', train)
+    #
+    # # cv.drawMatchesKnn expects list of lists as matches.
+    # img3 = cv.drawMatchesKnn(img1, kp1, img2, kp2, good, None, flags=cv.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
+    # plt.imshow(img3), plt.show()
